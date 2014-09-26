ed1bcd30ef0a2fa11231ecb1ed54f778d548e9c4
icasimpan <ismael.angelo@casimpan.com>
2014-09-26 08:17:12 +0800

    added custom stock input for analyst_recommends.sh & news.sh

--- news.sh
+++ news.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ticker="
+my_portfolio="
 CNPF
 COSCO
 DMPL
@@ -15,10 +15,11 @@ SMC
 FDC
 "
 
+ticker=$(echo $*|tr a-z A-Z)
+[[ "$ticker" = "" ]] && ticker=$my_portfolio
+
 for each_ticker in $ticker; do
   echo "[$each_ticker]"
   curl -s http://www.reuters.com/finance/stocks/$each_ticker.PS/key-developments|egrep "article|timestamp"|grep -v "Full Article"|grep -v stylesheet
   echo 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-  #grade=$(curl -s http://www.reuters.com/finance/stocks/overview?symbol=$each_ticker.PS|grep "Analyst Recommendation"|sed 's!.* title="\([0-9]*.[0-9]*\)" />!\1!g')
-  #[[ "$grade" != "" ]] && echo $grade || echo 'N/A'
 done
